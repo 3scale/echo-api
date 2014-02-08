78c36fa061c604d2ce6c5e832cf0a648b3c0d914
Michal Cichra <michal@o2h.cz>
2014-02-08 16:38:54 +0100

    async sinatra

--- echo_api.rb
+++ echo_api.rb
@@ -1,9 +1,17 @@
 #shotgun app.rb -p 9294
+
 require 'sinatra'
 require "json"
+
+require 'async-rack'
+require 'sinatra/async'
+register Sinatra::Async
+
+enable :logging
+
 def all_methods(path, opts = {}, &block)
-  get(path, opts, &block)
-  post(path, opts, &block)
+  aget(path, opts, &block)
+  apost(path, opts, &block)
   put(path, opts, &block)
   delete(path, opts, &block)
   patch(path, opts, &block)
@@ -17,11 +25,13 @@ end
 
 all_methods "/**" do
   r = request.body.rewind
-  return {
+  res = {
     method: request.request_method,
     path: request.path,
     args: request.query_string,
     body: request.body.read,
     headers: get_headers()
   }.to_json
+
+  body(res)
 end
