c288e1e7336614eaba40653e9bb82cc923bbff8e
Michal Cichra <michal@o2h.cz>
2014-02-14 13:16:17 +0100

    timer and random bytes api

--- echo_api.rb
+++ echo_api.rb
@@ -9,6 +9,8 @@ enable :logging
 
 configure { set :server, :puma }
 
+require 'securerandom'
+
 def all_methods(path, opts = {}, &block)
   get(path, opts, &block)
   post(path, opts, &block)
@@ -22,6 +24,40 @@ end
 def get_headers
   env.select {|k, v| k.start_with? 'HTTP_'}
 end
+
+@@random = Random.new
+@@random_files = Hash.new{|hash,size| hash[size] = @@random.bytes(size) }
+
+get '/size/:size' do |size|
+  size, unit = size.scan(/\d+|\D+/)
+
+  size = Integer(size)
+  unit = String(unit).downcase
+
+  multiplier = case unit
+                 when 'kb' then 1024
+                 when 'mb' then 1024*1024
+                 else 1
+               end
+
+  content_type 'application/octet-stream'
+  body @@random_files[size * multiplier]
+end
+
+
+get '/wait/:seconds' do |seconds|
+  duration = Float(seconds)
+  stream do |io|
+    increment = duration / 1000
+    1000.times do |i|
+      Kernel.sleep(increment)
+      io << "waited for #{increment}s\n"
+    end
+
+    io << "done waiting #{duration}"
+  end
+end
+
 
 all_methods "/**" do
   r = request.body.rewind
