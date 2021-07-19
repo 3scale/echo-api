136895cb863f10775c9546af9732a28abe3ae760
Krishna Harsha Voora <krishvoor@in.ibm.com>
2021-07-19 13:43:24 -0400

    Enable ppc64le in echo-api
    This PR enables ppc64le architecture in echo-api
    Travis job is triggered for every PR submitted
    
    Signed-off-by: Krishna Harsha Voora <krishvoor@in.ibm.com>

---
+++ .travis/build_ppc64le.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+
+# Start Echo API
+cd ..
+bundle exec rackup --port 3000 --host 0.0.0.0 --env production &
+
+# Sleep
+sleep 10
+
+# test Echo API
+curl --fail http://0.0.0.0:3000/test
